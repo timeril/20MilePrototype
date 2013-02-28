124c790784c3dd9a2b416966fbea0d4e95521117
Rodrigo Passos <rodrigopassos@mac.com>
2013-02-28 17:08:32 -0500

    Lots done on the results page

--- Build/assets/js/app.js
+++ Build/assets/js/app.js
@@ -1,10 +1,31 @@
 (function() {
 
   $(function() {
-    return $('#hw-nav a').on('click', function(event) {
+    $('#hw-nav a').on('click', function(event) {
       event.preventDefault();
       $('#hw-nav').removeClass().addClass('span12').addClass($(this).attr('href'));
       return $('#hw-nav').next().removeClass().addClass('span12').addClass($(this).attr('href'));
+    });
+    return $('.results-modal').on('show', function() {
+      var api;
+      api = $('.scrollable').scrollable();
+      return $(this).find('.items img').on('click', function() {
+        var img, thumb, url, wrap;
+        thumb = $(this);
+        if (thumb.parent().hasClass('active')) {
+          return;
+        }
+        url = $(this).data('src');
+        wrap = $('.modal-body').fadeTo('medium', 0.5);
+        img = new Image();
+        img.onload = function() {
+          wrap.fadeTo('fast', 1);
+          return wrap.find('img').attr('src', url);
+        };
+        img.src = url;
+        $('.items img').parent().removeClass('active');
+        return $(this).parent().addClass('active');
+      });
     });
   });
 
--- assets/js/app.coffee
+++ assets/js/app.coffee
@@ -4,3 +4,26 @@ $ ->
 		event.preventDefault()
 		$('#hw-nav').removeClass().addClass('span12').addClass($(@).attr('href'))
 		$('#hw-nav').next().removeClass().addClass('span12').addClass($(@).attr('href'))
+
+	$('.results-modal').on 'show', ()->
+		api = $('.scrollable').scrollable()
+		$(@).find('.items img').on 'click', ()->
+			thumb = $(@)
+
+			return if thumb.parent().hasClass('active')
+
+			url = $(@).data('src')
+
+			wrap = $('.modal-body').fadeTo('medium', 0.5)
+
+			img = new Image()
+
+			img.onload = ()->
+				wrap.fadeTo('fast', 1)
+				wrap.find('img').attr('src', url)
+
+			img.src = url
+
+			$('.items img').parent().removeClass('active')
+			$(@).parent().addClass('active')
+
